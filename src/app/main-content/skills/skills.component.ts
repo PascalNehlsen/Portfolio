2b70016d5af37326400b9a9b126370d8c3811e23
Pascal Nehlsen <pascaldev92@gmail.com>
2024-07-29 14:34:23 +0200

    animate atf slider

--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PortfolioService } from './../../portfolio.service';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrl: './skills.component.scss',
 })
 export class SkillsComponent {
+  constructor(public PortfolioService: PortfolioService) {}
 
+  showImage(imageName: string) {
+    this.PortfolioService.showImage(imageName);
+  }
+
+  hideImage() {
+    this.PortfolioService.hideImage();
+  }
+
+  get activeImage(): string {
+    return this.PortfolioService.getActiveImage();
+  }
 }
