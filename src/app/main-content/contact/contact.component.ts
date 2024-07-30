92707e445b0c9a28ebeee3cfeec76a9bb712cfae
Pascal Nehlsen <pascaldev92@gmail.com>
2024-07-30 22:32:54 +0200

    form checkbox hover and click to enable submit button

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
@@ -15,9 +16,47 @@ export class ContactComponent {
     message: '',
   };
 
+  checkboxSrc: string = './assets/img/icons/checkbox.png';
+  isChecked: boolean = false;
+  isFormEnabled: boolean = false;
+
   onSubmit(ngForm: NgForm) {
     if (ngForm.valid && ngForm.submitted) {
       console.log(this.contactData);
     }
+  }
+
+  checkboxEnterMouse() {
+    if (this.isChecked) {
+      this.checkboxSrc = './assets/img/icons/checkbox-hover-checked.png';
+    } else {
+      this.checkboxSrc = './assets/img/icons/checkbox-hover.png';
+    }
+  }
+
+  checkboxLeaveMouse() {
+    if (this.isChecked) {
+      this.checkboxSrc = './assets/img/icons/checkbox-checked.png';
+    } else {
+      this.checkboxSrc = './assets/img/icons/checkbox.png';
+    }
+  }
+
+  checkboxOnClick() {
+    this.isChecked = !this.isChecked;
+    this.checkboxEnterMouse();
+    if (this.checkboxSrc === './assets/img/icons/checkbox-hover-checked.png') {
+      this.enableFormBtn();
+    } else {
+      this.disableFormBtn();
+    }
+  }
+
+  enableFormBtn() {
+    this.isFormEnabled = true;
+  }
+
+  disableFormBtn() {
+    this.isFormEnabled = false;
   }
 }
