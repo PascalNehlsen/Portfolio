3de4b4c4dd62aefaa65758d61467a6c0caf1f932
Pascal Nehlsen <pascaldev92@gmail.com>
2024-08-01 15:56:05 +0200

    render projects

--- src/app/main-content/projects/dialog/dialog.component.ts
+++ src/app/main-content/projects/dialog/dialog.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dialog',
   standalone: true,
   imports: [],
   templateUrl: './dialog.component.html',
-  styleUrl: './dialog.component.scss'
+  styleUrl: './dialog.component.scss',
 })
 export class DialogComponent {
-
+  @Input() singleproject!: {
+    name: string;
+    tools: string[];
+    description: string;
+    symbols: string[];
+    image: string;
+    imageDialog?: string;
+  };
 }
--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -13,6 +13,55 @@ import { PortfolioService } from '../../portfolio.service';
 export class ProjectsComponent {
   constructor(public PortfolioService: PortfolioService) {}
 
+  projects: {
+    name: string;
+    tools: string[];
+    description: string;
+    symbols: string[];
+    image: string;
+    imageDialog?: string;
+  }[] = [
+    {
+      name: 'join',
+      image: './assets/img/projects/join-small.png',
+      description:
+        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      symbols: [
+        './assets/img/icons/css-green.png',
+        './assets/img/icons/html-green.png',
+        './assets/img/icons/javascript-green.png',
+        './assets/img/icons/firebase-green.png',
+      ],
+      tools: ['CSS', 'HTML', 'JavaScript', 'Firebase'],
+    },
+    {
+      name: 'elpolloloco',
+      image: './assets/img/projects/elpolloloco-small.png',
+      description:
+        'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      symbols: [
+        './assets/img/icons/css-green.png',
+        './assets/img/icons/html-green.png',
+        './assets/img/icons/javascript-green.png',
+        './assets/img/icons/firebase-green.png',
+      ],
+      tools: ['CSS', 'HTML', 'JavaScript', 'Firebase'],
+    },
+    {
+      name: 'pokedex',
+      image: './assets/img/projects/pokedex-small.png',
+      description:
+        'This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization.',
+      symbols: [
+        './assets/img/icons/css-green.png',
+        './assets/img/icons/html-green.png',
+        './assets/img/icons/javascript-green.png',
+        './assets/img/icons/firebase-green.png',
+      ],
+      tools: ['CSS', 'HTML', 'JavaScript', 'Firebase'],
+    },
+  ];
+
   showImage(imageName: string) {
     this.PortfolioService.showImage(imageName);
   }
@@ -23,5 +72,9 @@ export class ProjectsComponent {
 
   get activeImage(): string {
     return this.PortfolioService.getActiveImage();
+  }
+
+  openDialog(i: number) {
+    i = 0;
   }
 }
