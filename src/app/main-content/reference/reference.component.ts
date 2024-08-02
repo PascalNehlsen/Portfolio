9f756d50fd0022ae81b232836ebeb95c02bf7a3e
Pascal Nehlsen <pascaldev92@gmail.com>
2024-08-02 23:03:40 +0200

    adjust component structure

--- src/app/main-content/reference/reference.component.ts
+++ src/app/main-content/reference/reference.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { CarouselComponent } from './carousel/carousel.component';
 
 @Component({
   selector: 'app-reference',
   standalone: true,
-  imports: [CarouselComponent],
+  imports: [],
   templateUrl: './reference.component.html',
   styleUrl: './reference.component.scss',
 })
